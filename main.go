59799334d45a8237856c9fbc268518bbefffbc68
Vikuuu <adivik672@gmail.com>
2025-01-11 18:46:29 +0530

    refac

--- main.go
+++ main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
-	"io"
 	"log"
 	"net"
+)
+
+const (
+	CONN_HOST = "localhost"
+	CONN_PORT = "3333"
+	CONN_TYPE = "tcp"
 )
 
 func main() {
 	// Listen on TCP post 8080 on all available unicast and
 	// anycast IP addresses of the local system.
-	l, err := net.Listen("tcp", ":8080")
+	l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -24,10 +29,17 @@ func main() {
 		// The loop then returns to accepting, so that
 		// multiple connections may be served concurrently.
 		go func(c net.Conn) {
+			// Make a buffer to hold incoming data.
+			buf := make([]byte, 1024)
+			// Read the incoming connection into the buffer.
+			_, err := conn.Read(buf)
+			if err != nil {
+				log.Fatal(err)
+			}
+			// Send a response back to person contacting us.
+			conn.Write([]byte(buf))
 			// Shut down the connection.
-			defer c.Close()
-			// Echo all incoming data.
-			io.Copy(c, c)
+			c.Close()
 		}(conn)
 	}
 }
